a032d4ad8929a6687c5e8e855d51103a4ce9fc36
Thomas Seyroles <seytom00@gmail.com>
2026-01-07 11:27:35 +0100

    feat: Add mobile sidebar close icon with click functionality and styling.

--- src/main.js
+++ src/main.js
@@ -600,7 +600,8 @@ function init() {
   document.querySelector("#app").innerHTML = `
     <div class="wrapper">
       <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark" id="sidenav-main">
-        <div class="sidenav-header">
+        <div class="sidenav-header position-relative">
+          <i class="material-icons p-2 position-absolute top-0 end-0 cursor-pointer text-white opacity-5 d-none d-xl-none" aria-hidden="true" id="iconSidenav">close</i>
           <a class="navbar-brand m-0" href="#">
             <span class="ms-1 font-weight-bold text-white">SAE303 Dashboard</span>
           </a>
@@ -836,6 +837,13 @@ function init() {
   if (toggleSidenav) {
     toggleSidenav.addEventListener("click", () => {
       sidenav.classList.toggle("show");
+    });
+  }
+
+  const iconSidenav = document.getElementById("iconSidenav");
+  if (iconSidenav) {
+    iconSidenav.addEventListener("click", () => {
+      sidenav.classList.remove("show");
     });
   }
 
